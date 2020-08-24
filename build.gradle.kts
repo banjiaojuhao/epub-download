ce14bfd1073098cf87476720105c45205fed0461
banjiaojuhao <banjiaojuhao@qq.com>
2020-08-25 02:10:40 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+
+    id("com.github.johnrengelman.shadow").version("5.2.0")
+
+    // Apply the application plugin to add support for building a CLI application.
+    application
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+val vertxVersion = "3.9.2"
+val jsoupVersion = "1.12.1"
+val coroutineVersion = "1.3.9"
+val reflectionVersion = "1.4.0"
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+
+    implementation("io.vertx:vertx-core:$vertxVersion")
+    implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-web-client:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+
+    implementation("org.jsoup:jsoup:$jsoupVersion")
+
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$reflectionVersion")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClassName = "epub.download.AppKt"
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
+
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to application.mainClassName))
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ src/main/kotlin/epub/download/App.kt
@@ -0,0 +1,97 @@
+package epub.download
+
+import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.client.WebClient
+import io.vertx.kotlin.core.closeAwait
+import io.vertx.kotlin.core.file.existsAwait
+import io.vertx.kotlin.core.file.mkdirAwait
+import io.vertx.kotlin.core.file.readFileAwait
+import io.vertx.kotlin.core.file.writeFileAwait
+import io.vertx.kotlin.ext.web.client.sendAwait
+import io.vertx.kotlin.ext.web.client.webClientOptionsOf
+import kotlinx.coroutines.runBlocking
+import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
+import java.net.URI
+import java.nio.file.Path
+
+
+data class Item(val href: String, val type: String)
+
+/**
+ * todo
+ * 自动添加、生成META-INF、mimetype
+ * 自动打包zip并且重命名为“书名 - 作者.epub”
+ * 用户手动输入下载地址
+ * 下载完成删除临时目录
+ */
+fun main(args: Array<String>) = runBlocking<Unit> {
+    val url = "http://reader.epubee.com/books/mobile/5f/5f80cfe69440056dc623f051c2f76246/"
+    val bookId = url.substringAfter("mobile/").substringAfter("/").substringBefore("/")
+    val rootPath = Path.of("cache", bookId)
+
+    val httpOptions = webClientOptionsOf(
+            userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+            trustAll = true,
+            followRedirects = true,
+            verifyHost = false,
+            keepAlive = true,
+            tryUseCompression = true)
+
+    val vertx = Vertx.vertx()
+    val webClient = WebClient.create(vertx, httpOptions)!!
+    val fileSystem = vertx.fileSystem()
+
+    if (!fileSystem.existsAwait(bookId)) {
+        fileSystem.mkdirAwait(bookId)
+    }
+
+
+    val pathOPF = rootPath.resolve("content.opf").toString()
+
+    val content = if (!fileSystem.existsAwait(pathOPF)) {
+        val response = webClient.getAbs(url + "content.opf")
+                .sendAwait()
+        val contentBuffer = response.bodyAsBuffer()
+        val content = contentBuffer.toString()
+        fileSystem.writeFileAwait(pathOPF, contentBuffer)
+        content
+    } else {
+        fileSystem.readFileAwait(pathOPF).toString()
+    }
+
+    val dom = Jsoup.parse(content, url, Parser.xmlParser())
+    val resourceList = dom.select("package > manifest > item").map {
+        Item(it.attr("href"), it.attr("media-type"))
+    }
+
+    for (index in resourceList.indices) {
+        val item = resourceList[index]
+        println("${index / resourceList.size}\t${item.href}")
+        val path = rootPath.resolve(item.href).toString()
+        val html = if (!fileSystem.existsAwait(path)) {
+            val itemBuffer = webClient.getAbs(URI(url).resolve(item.href).toString())
+                    .sendAwait()
+                    .bodyAsBuffer()
+            fileSystem.writeFileAwait(path, itemBuffer)
+            itemBuffer
+        } else {
+            fileSystem.readFileAwait(path)
+        }
+
+        if (item.type == "application/xhtml+xml") {
+            val parse = Jsoup.parse(html.toString())
+            val contentHtml = parse.select(".readercontent-inner").html()
+            parse.select("body").html(contentHtml)
+            val newHtml = parse.html()
+            println()
+            fileSystem.writeFileAwait(path, Buffer.buffer(newHtml))
+        }
+    }
+
+
+
+    webClient.close()
+    vertx.closeAwait()
+}
